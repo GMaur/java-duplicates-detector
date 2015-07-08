fcf34c88fa50dcd048a432eaf6d5209f9277135b
Alvaro Garcia <derecerca@gmail.com>
2015-07-08 18:36:40 +0200

    refactor: inline variables

--- src/test/java/com/gmaur/tool/java/duplicatedetector/RepeatedFinderShould.java
+++ src/test/java/com/gmaur/tool/java/duplicatedetector/RepeatedFinderShould.java
@@ -10,8 +10,7 @@ public class RepeatedFinderShould {
 
 	@Test
 	public void find_duplicates_in_string_and_integer_constants() throws IllegalAccessException {
-		RepeatedFinder sut = sutWith(RepeatedConstants.class);
-		MultiMap<Object, String> duplicates = sut.findDuplicates();
+		MultiMap<Object, String> duplicates = sutWith(RepeatedConstants.class).findDuplicates();
 
 		for(Object current : duplicates.all()){
 			System.out.println("found value = '" + current+"', with variables = " + duplicates.get(current));
@@ -24,16 +23,14 @@ public class RepeatedFinderShould {
 
 	@Test
 	public void not_find_duplicates_in_attributes() throws IllegalAccessException {
-		RepeatedFinder sut = sutWith(AttributesButNoConstants.class);
-		MultiMap<Object, String> duplicates = sut.findDuplicates();
+		MultiMap<Object, String> duplicates = sutWith(AttributesButNoConstants.class).findDuplicates();
 
 		assertThat(duplicates.size(), is(0));
 	}
 
 	@Test
 	public void not_find_duplicates_when_there_are_no_fields() throws IllegalAccessException {
-		RepeatedFinder sut = sutWith(NoConstants.class);
-		MultiMap<Object, String> duplicates = sut.findDuplicates();
+		MultiMap<Object, String> duplicates = sutWith(NoConstants.class).findDuplicates();
 
 		assertThat(duplicates.size(), is(0));
 	}
