3d4dcc4931608a0298f0ef0b3c5a31be4fbb81e6
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2015-07-14 20:22:50 +0200

    defect: wrong copyright

--- src/test/java/com/gmaur/tool/java/duplicatedetector/RepeatedConstants.java
+++ src/test/java/com/gmaur/tool/java/duplicatedetector/RepeatedConstants.java
@@ -1,12 +1,3 @@
-/**
- * $Id$
- *
- * @author alvgarcia
- * @date 08/07/2015 18:30
- * Copyright (C) 2015 Scytl Secure Electronic Voting SA
- * All rights reserved.
- */
-
 package com.gmaur.tool.java.duplicatedetector;
 
 
